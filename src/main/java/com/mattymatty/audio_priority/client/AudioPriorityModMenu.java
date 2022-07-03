9b0ba18d69898760cd3879d3387086dc9226e6fe
mattymatty <matteo.francesconi@freemails.it>
2022-07-03 21:08:40 +0200

    Fix Bad ModMenu API, Force Modmenu and fabric-API to not be downloaded on testing

--- src/main/java/com/mattymatty/audio_priority/client/AudioPriorityModMenu.java
+++ src/main/java/com/mattymatty/audio_priority/client/AudioPriorityModMenu.java
@@ -1,15 +1,12 @@
 package com.mattymatty.audio_priority.client;
 
 import com.mattymatty.audio_priority.screen.ConfigScreen;
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 
 public class AudioPriorityModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-
-            return new ConfigScreen(parent);
-        };
+        return ConfigScreen::new;
     }
 }
